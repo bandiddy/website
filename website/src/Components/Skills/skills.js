7e23cabc1197723f402efe128ec54509cee7290d
bandiddy <38272680+bandiddy@users.noreply.github.com>
2019-05-22 23:30:42 -0700

    skills

--- website/src/Components/Skills/skills.js
+++ website/src/Components/Skills/skills.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
 const styles = theme => ({
@@ -19,32 +18,26 @@ function CenteredGrid(props) {
       <Grid item xs={3} md={3}>CSS3</Grid>
       <Grid item xs={3} md={3}>React.js</Grid>
       <Grid item xs={3} md={3}>Node.js</Grid>
-      {/* <h4>Scripts/UI</h4>
-          <div>
-            <p>Javascirpt</p>
-            <p>Jquery</p>
-            <p>AJAX</p>
-            <p>Bootstrap</p>
-            <p>Materialize</p>
-            <p>Bulma</p>
-            <p>Materialize</p>
-            <p>Material UI</p>
-          </div>
-          <h4>Database & ORM</h4>
-          <div>
-            <p>MySQL</p>
-            <p>MongoDB</p>
-            <p>Firebase</p>
-          </div>
-          <h4>Versioning & Deployment</h4>
-          <div>
-            <p>Git</p>
-            <p>Heroku</p>
-            <p>Jest</p>
-          </div> */}
 
+      <Grid item xs={12} sm={12}><h4>Scripts/UI</h4></Grid>
+      <Grid item xs={4} md={4}>Javascirpt</Grid>
+      <Grid item xs={4} md={4}>JQuery</Grid>
+      <Grid item xs={4} md={4}>AJAX</Grid>
+      <Grid item xs={3} md={3}>Bootstrap</Grid>
+      <Grid item xs={3} md={3}>Materialize</Grid>
+      <Grid item xs={3} md={3}>Bulma</Grid>
+      <Grid item xs={3} md={3}>Material UI</Grid>
 
+      <Grid item xs={12} sm={12}><h4>Database & ORM</h4></Grid>
+      <Grid item xs={4} md={4}>MySQL</Grid>
+      <Grid item xs={4} md={4}>MongoDB</Grid>
+      <Grid item xs={4} md={4}>Firebase</Grid>
 
+      <Grid item xs={12} sm={12}><h4>Versioning & Deployment</h4></Grid>
+      <Grid item xs={4} md={4}>Git</Grid>
+      <Grid item xs={4} md={4}>Heroku</Grid>
+      <Grid item xs={4} md={4}>Jest</Grid>
+      
     </Grid>
 
   );
