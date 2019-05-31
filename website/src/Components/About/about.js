70a63beb9ca9dd49767d91f8f4f4c3b3c53b6200
bandiddy <38272680+bandiddy@users.noreply.github.com>
2019-05-31 00:34:03 -0700

    more

--- website/src/Components/About/about.js
+++ website/src/Components/About/about.js
@@ -4,58 +4,77 @@ import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import { blue, green } from '@material-ui/core/colors';
-import ScrollableAnchor from 'react-scrollable-anchor'
+import ScrollableAnchor from 'react-scrollable-anchor';
+import alvin from '../../images/alvin.jpg';
+import { Parallax, Background } from 'react-parallax';
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
-    padding: 200,  
+    padding: 100,
+    
   },
   paper: {
     padding: 100,
-    
   },
 });
 
 function FullWidthGrid(props) {
   const { classes } = props;
 
+  
+
   return (
-    <div className={classes.root} >
+    
+    <div  >
+      <Parallax
+            className={classes.root}
+            blur={ 5 }
+            bgImage={require('../../images/alvin.jpg')}
+            bgImageAlt="alvin"
+            strength={400}
+        >
     <ScrollableAnchor id="about">
       <Grid container spacing={24}>
         <Grid item xs={6} align="center">
           {/* <Paper className={classes.paper}> */}
-          <div >
+          {/* <div >
            <h1>Alvin Joseph Garcia</h1>
            <br></br>
            <br></br>
            <br></br>
            <h2>I'm a Web Developer!</h2>
-           </div>
+           </div> */}
            
         {/* </Paper> */}
         </Grid>
         <Grid item xs={6} align="center">
         <div className={classes.paper}>
         {/* <Paper className={classes.paper}> */}
+        <h1>Alvin Joseph Garcia</h1>
         <div>Enthusiastic and success driven web developer, eager to collaborate on 
            constantly changing projects with a focus on front and back end. 
             With a passion to learn and grow with ever changing technology, I am committed to creating applications 
-            to help benefit and empower people in a meaningful way
+            to help benefit and empower people in a meaningful way.
           </div>
+          <h2>I'm a Web Developer!</h2>
         {/* </Paper> */}
         </div>
         </Grid>
       
       </Grid>
     </ScrollableAnchor>
+    
+    
+    
+    </Parallax>
     </div>
   );
 }
 
 FullWidthGrid.propTypes = {
   classes: PropTypes.object.isRequired,
+  
 };
 
 export default withStyles(styles)(FullWidthGrid);
